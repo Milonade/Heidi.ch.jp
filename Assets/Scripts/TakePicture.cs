09591a5575b2a1f0df53a3f1384b4a3bd19aee5b
Jasmine Molano <jasmine.molanocorreia@etu.hesge.ch>
2024-06-06 09:27:38 +0200

    Added asset

---
+++ Assets/Scripts/TakePicture.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakePicture : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject sphere;
+
+    // i want to change the color of the cube when two other cubes collides
+    void OnTriggerEnter(Collider other)
+    {
+        //if (collision.gameObject.name == "Cube1" && collision.gameObject.name == "Cube2")
+        {
+            cube.GetComponent<Renderer>().material.color = Color.blue;
+            Invoke("SpawnSphere", 1.5f);
+
+        }
+    }
+
+    void SpawnSphere()
+    {
+        sphere.SetActive(true);
+
+    }
+
+}
