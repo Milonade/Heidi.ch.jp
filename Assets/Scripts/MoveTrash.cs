f5a7f9470961caef21664dcbdcfc427c27a756f6
Jasmine Molano <jasmine.molanocorreia@etu.hesge.ch>
2024-06-12 09:37:28 +0200

    planet colision

---
+++ Assets/Scripts/MoveTrash.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTrash : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTownObjects();
+    }
+    // get objects in the scene and make them vibrate
+    public void MoveTownObjects()
+    {
+        // get all the objects in the scene
+        GameObject[] trashObjects = GameObject.FindGameObjectsWithTag("Town");
+        
+        // for each object in the scene
+        foreach (GameObject trashObject in trashObjects)
+        {
+            // make the object drift away
+            trashObject.transform.position += new Vector3(0, 0, 0.01f);
+        }
+    }
+
+    // if objects collide with the world sphere invert the direction
+    void OnTriggerEnter(Collider other)
+    {
+        // if the object collides with the world sphere
+        if (other.gameObject.CompareTag("World"))
+        {
+            // invert the direction of the object
+           other.gameObject.transform.position -= new Vector3(0,0,0.1f);
+        }
+    }
+
+}
